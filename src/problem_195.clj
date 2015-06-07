c86497924190836146b0d714dbd28ff9545fcb7a
Fabianus Mulawadi <fabianus@gmail.com>
2015-06-08 00:08:54 +0800

    Solved #195

--- src/problem_195.clj
+++ src/problem_195.clj
@@ -1,1 +1,11 @@
- (ns problem-195)
+(ns problem-195)
+
+(defn __ [N]
+  (letfn [(aux [l r]
+               (if (and (= l N) (= (inc r) N))              ; last character is always right paren
+                 [[\)]]
+                 (let [lx (if (< l N) (map (partial cons \() (aux (inc l) r)) [])
+                       rx (if (< r l) (map (partial cons \)) (aux l (inc r))) [])]
+                   (concat lx rx))))]
+    (if (zero? N) #{""}                                     ; special case
+                  (set (map #(apply str %) (aux 0 0))))))
