96788b9e93a8aa10bfe436d9c1dd50374c265962
Fabianus Mulawadi <fabianus@gmail.com>
2015-06-15 23:42:50 +0800

    Solved #94 and #106

--- src/problem_094.clj
+++ src/problem_094.clj
@@ -1,1 +1,73 @@
- (ns problem-094)
+(ns problem-094)
+
+(defn __ [matrix]
+  (let [nrow (count matrix)
+        ncol (count (first matrix))
+        nbor (fn [x y dx dy]
+               (let [x' (+ x dx)
+                     y' (+ y dy)]
+                 (if (and (< -1 x' ncol)
+                          (< -1 y' nrow))
+                   (cond
+                     (= dx dy 0) 0
+                     (= \# (get-in matrix [y' x'])) 1
+                     :else 0)
+                   0)))
+        nbors (fn [x y]
+                (apply + (for [dx [-1 0 1]
+                               dy [-1 0 1]]
+                           (nbor x y dx dy))))
+        next-state (fn [state nb-count]
+                     (cond
+                       (= nb-count 3) true                  ; rule #4
+                       (< nb-count 2) false                 ; rule #1
+                       (< nb-count 4) state                 ; rule #2
+                       :else false))]                       ; rule #3
+    (for [y (range nrow)]
+      (apply str
+        (for [x (range ncol)]
+          (let [cur-state (= \# (get-in matrix [y x]))
+                nb-count (nbors x y)]
+            (if (next-state cur-state nb-count) \# \space)))))))
+
+(assert
+  (= (__ ["      "
+          " ##   "
+          " ##   "
+          "   ## "
+          "   ## "
+          "      "])
+     ["      "
+      " ##   "
+      " #    "
+      "    # "
+      "   ## "
+      "      "]))
+
+
+(assert
+  (= (__ ["     "
+          "     "
+          " ### "
+          "     "
+          "     "])
+     ["     "
+      "  #  "
+      "  #  "
+      "  #  "
+      "     "]))
+
+(assert
+  (= (__ ["      "
+          "      "
+          "  ### "
+          " ###  "
+          "      "
+          "      "])
+     ["      "
+      "   #  "
+      " #  # "
+      " #  # "
+      "  #   "
+      "      "]))
+
--- src/problem_106.clj
+++ src/problem_106.clj
@@ -1,1 +1,17 @@
- (ns problem-106)
+(ns problem-106)
+
+(defn __ [source target]
+  (let [halve (fn [x] (/ x 2))
+        add-2 (fn [x] (+ x 2))
+        twice (fn [x] (* x 2))
+        enque (fn [q x d]
+                (let [q' (->> q
+                              (cons [(add-2 x) d])
+                              (cons [(twice x) d]))]
+                  (if (odd? x) q' (cons [(halve x) d] q'))))]
+    (loop [q (enque (list [source 1]) source 2)]
+      (let [[x d] (last q)
+            q' (butlast q)]
+        (if (= x target)
+          d
+          (recur (enque q' x (inc d))))))))
